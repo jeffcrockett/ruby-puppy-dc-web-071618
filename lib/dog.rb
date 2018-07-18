53b3c5a0a26e8b6309668f3a5cdb739bfdb3371d
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-18 19:52:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  @@all = []
 
+  attr_accessor :name 
+
+  def initialize(name)
+    @name = name 
+
+  def self.clear_all 
+    @all.clear 
+  end
+
+  def self.all 
+    @all.each do |dog| puts dog end 
+  end
 end
